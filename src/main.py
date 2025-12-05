f76ac3128a3cda07a9e161cccf58f9d6d6ab6f3d
Walter Fritsche <63881577+vosram@users.noreply.github.com>
2025-12-05 09:42:39 -0600

    CH2 Nodes L1 TextNode

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from textnode import TextNode, TextType
+
+
+def main():
+    text_node = TextNode(
+        "This is some anchor text", TextType.LINK, "https://www.boot.dev"
+    )
+    print(text_node)
+
+
+main()
