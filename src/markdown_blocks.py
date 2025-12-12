c06b59b7dc0ac85f993caf4add9f5c92c09ed419
Walter Fritsche <63881577+vosram@users.noreply.github.com>
2025-12-12 11:15:35 -0600

    Add block to blocktype function and test CH4L2

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,7 @@
+from enum import Enum
+import re
+
+
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
     filtered_blocks = []
@@ -7,3 +11,45 @@ def markdown_to_blocks(markdown):
         block = block.strip()
         filtered_blocks.append(block)
     return filtered_blocks
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+
+def block_to_block_type(block: str):
+    # according to the docs, re.match searches the start or the line
+    # so i don't think i need a ^ char in front of the regex pattern
+    # to specify the pattern is at the start of the text
+    # https://docs.python.org/3/library/re.html#re.match
+
+    lines = block.split("\n")
+
+    if re.match(r"#{,6}\s", block):
+        return BlockType.HEADING
+    elif len(lines) > 1 and block.startswith("```") and block.endswith("```"):
+        return BlockType.CODE
+    elif block.startswith(">"):
+        for line in lines:
+            if not line.startswith(">"):
+                return BlockType.PARAGRAPH
+        return BlockType.QUOTE
+    elif block.startswith("- "):
+        for line in lines:
+            if not line.startswith("- "):
+                return BlockType.PARAGRAPH
+        return BlockType.UNORDERED_LIST
+    elif block.startswith("1. "):
+        i = 1
+        for line in lines:
+            if not line.startswith(f"{i}. "):
+                return BlockType.PARAGRAPH
+            i += 1
+        return BlockType.ORDERED_LIST
+    else:
+        return BlockType.PARAGRAPH
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,6 +1,6 @@
 import unittest
 
-from markdown_blocks import markdown_to_blocks
+from markdown_blocks import markdown_to_blocks, block_to_block_type, BlockType
 
 
 class TestBlockMarkdown(unittest.TestCase):
@@ -66,6 +66,120 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+
+    def test_block_to_block_type(self):
+        md = """
+# Cool heading 1
+
+Some lovely paragraph **content** with great information
+
+- ordered topic
+- with another topic
+- and yet another topic
+
+## heading for the unordered?
+
+1. yeah you guessed it
+2. this would be an ordered list
+3. Ain't that something
+
+```
+def lets_start_coding(here):
+    pass
+```
+
+## last words here
+
+> And thus concludes with my final quote
+"""
+
+        blocks = markdown_to_blocks(md)
+        self.assertListEqual(
+            [
+                block_to_block_type(blocks[0]),
+                block_to_block_type(blocks[1]),
+                block_to_block_type(blocks[2]),
+                block_to_block_type(blocks[3]),
+                block_to_block_type(blocks[4]),
+                block_to_block_type(blocks[5]),
+                block_to_block_type(blocks[6]),
+                block_to_block_type(blocks[7]),
+            ],
+            [
+                BlockType.HEADING,
+                BlockType.PARAGRAPH,
+                BlockType.UNORDERED_LIST,
+                BlockType.HEADING,
+                BlockType.ORDERED_LIST,
+                BlockType.CODE,
+                BlockType.HEADING,
+                BlockType.QUOTE,
+            ],
+        )
+
+    def test_block_to_block_type_wrong_syntax(self):
+        md = """
+# Cool heading 1
+
+Some lovely paragraph **content** with great information
+
+- ordered topic
+with another topic
+- and yet another topic
+
+## heading for the unordered?
+
+1. yeah you guessed it
+7. this would be an ordered list
+3. Ain't that something
+
+```
+def lets_start_coding(here):
+    pass
+```
+
+## last words here
+
+> And thus concludes with my final quote
+"""
+
+        blocks = markdown_to_blocks(md)
+        self.assertListEqual(
+            [
+                block_to_block_type(blocks[0]),
+                block_to_block_type(blocks[1]),
+                block_to_block_type(blocks[2]),
+                block_to_block_type(blocks[3]),
+                block_to_block_type(blocks[4]),
+                block_to_block_type(blocks[5]),
+                block_to_block_type(blocks[6]),
+                block_to_block_type(blocks[7]),
+            ],
+            [
+                BlockType.HEADING,
+                BlockType.PARAGRAPH,
+                BlockType.PARAGRAPH,
+                BlockType.HEADING,
+                BlockType.PARAGRAPH,
+                BlockType.CODE,
+                BlockType.HEADING,
+                BlockType.QUOTE,
+            ],
+        )
+
+    def test_block_to_block_types(self):
+        block = "# heading"
+        self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+        block = "```\ncode\n```"
+        self.assertEqual(block_to_block_type(block), BlockType.CODE)
+        block = "> quote\n> more quote"
+        self.assertEqual(block_to_block_type(block), BlockType.QUOTE)
+        block = "- list\n- items"
+        self.assertEqual(block_to_block_type(block), BlockType.UNORDERED_LIST)
+        block = "1. list\n2. items"
+        self.assertEqual(block_to_block_type(block), BlockType.ORDERED_LIST)
+        block = "paragraph"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
 
 
 if __name__ == "__main__":
